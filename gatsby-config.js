687557df97ef7f8e659358c68f6d476c884eaea6
Luca Ramundo <0xc14m1z@gmail.com>
2020-01-03 23:17:13 +0000

    Add styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: `0xc14m1z`,
-    description: `I'm an experienced Frontend Developer with more than ten years of full stack projects behind me as a freelance.`,
-    author: `Luca Ramundo`
+    title: '0xc14m1z',
+    description: "I'm an experienced Frontend Developer with more than ten years of full stack projects behind me as a freelance.",
+    author: 'Luca Ramundo'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-styled-components',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
