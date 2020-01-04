778da4de7c2102cd589e52f3a9dee2ba0a1caba4
Luca Ramundo <0xc14m1z@gmail.com>
2020-01-04 00:34:17 +0000

    Make file names consistent

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import t from 'prop-types'
+import styled from 'styled-components'
+
+import { Link } from 'gatsby'
+import Icon from './Icon'
+
+const Container = styled.header `
+  z-index: 1;
+  position: sticky;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 50px;
+
+  margin-bottom: 2em;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0px 1.5em;
+  box-sizing: border-box;
+
+  border-bottom: 1px solid ${props => props.theme.colors.border};
+  background: white;
+`
+
+const Title = styled.span `
+  color: ${props => props.theme.colors.text};
+  font-size: 1em;
+  font-weight: bold;
+`
+
+const Home = () => (
+  <Link to="/" title="Home">
+    <Icon.Home />
+  </Link>
+)
+
+const Resume = () => (
+  <Link to="/resume" title="Résumé">
+    <Icon.Info />
+  </Link>
+)
+
+const Header = ({ title }) => (
+  <Container>
+    <Home />
+    <Title>{ title }</Title>
+    <Resume />
+  </Container>
+)
+
+Header.propTypes = {
+  title: t.string.isRequired
+}
+
+Header.defaultProps = {
+  title: ''
+}
+
+export default Header
