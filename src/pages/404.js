8134bf24ac695f2bb7650e50fd82c498693d4a2c
Luca Ramundo <0xc14m1z@gmail.com>
2020-01-04 02:03:27 +0000

    Refine 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
+import Icon from '../components/Icon'
+import Message from '../components/Message'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-
+    <Message icon={ <Icon.NotFound /> }>
+      Oh no! The page you were looking for was not found...
+    </Message>
   </Layout>
 )
 
