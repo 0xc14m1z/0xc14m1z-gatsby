64dc3caa27ec069ca35f6b3a7f6b508e98b5a0dc
Luca Ramundo <0xc14m1z@gmail.com>
2020-01-04 01:15:44 +0000

    Refine application layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,9 @@
  */
 
 import React from 'react'
-import PropTypes from 'prop-types'
+import t from 'prop-types'
+import styled from 'styled-components'
+
 import { useStaticQuery, graphql } from 'gatsby'
 
 import GlobalStyle from './GlobalStyle'
@@ -14,6 +16,15 @@ import Theme from './Theme'
 
 import Header from './Header'
 import Footer from './Footer'
+
+const Container = styled.div `
+  max-width: ${ props => props.theme.viewport.maxWidth };
+  margin: auto;
+`
+
+const Main = styled.main `
+  padding: 0 1.5em;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -29,24 +40,17 @@ const Layout = ({ children }) => {
   return (
     <Theme>
       <GlobalStyle />
-      <Header title={ data.site.siteMetadata.title } />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0
-        }}
-      >
-        <main>{children}</main>
-      </div>
-      <Footer />
+      <Container>
+        <Header title={ data.site.siteMetadata.title } />
+        <Main>{ children }</Main>
+        <Footer />
+      </Container>
     </Theme>
   )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: t.node.isRequired
 }
 
 export default Layout
